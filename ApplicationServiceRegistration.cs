1d6e89d7c19a63c09b575b0c68dde3605b02db4b
Quentin Couissinier <reliabledevelopment@outlook.com>
2022-01-07 14:06:37 +0100

    Init

---
+++ ApplicationServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using ValidationBehaviour_toolsBasics.Behavior;
+
+namespace ValidationBehaviour_toolsBasics
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationService(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+            return services;
+        }
+    }
+}
